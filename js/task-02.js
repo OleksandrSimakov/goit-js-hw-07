9eb7f4814955a92ec2d78ac205441a8ea9cbb1c2
Oleksandr Simakov <simakov.mailbox@gmail.com>
2021-04-08 16:12:22 +0300

    corrects task2

--- js/task-02.js
+++ js/task-02.js
@@ -8,9 +8,24 @@ const ingredients = [
 ];
 
 
-const ingredientItemList = [...ingredients].forEach(ingredient => {
-    const ingredientItem = document.createElement('li');
-    ingredientItem.textContent = ingredient;
-    const ingredientsList = document.querySelector('#ingredients')
-    ingredientsList.appendChild(ingredientItem)
+// const ingredientItemList = [...ingredients].forEach(ingredient => {
+//     const ingredientItem = document.createElement('li');
+//     ingredientItem.textContent = ingredient;
+//     const ingredientsList = document.querySelector('#ingredients')
+//     ingredientsList.appendChild(ingredientItem)
+// })
+
+const ingredientsItems = [];
+
+ingredients.map((ingredient) => {
+  const ingredientItem = document.createElement('li');
+  ingredientItem.textContent = ingredient;
+  ingredientsItems.push(ingredientItem);
 })
+
+// console.log(ingredientsItems)
+
+const ingredientsList = document.querySelector('#ingredients')
+// console.log(ingredientsList)
+
+ingredientsList.append(...ingredientsItems)
